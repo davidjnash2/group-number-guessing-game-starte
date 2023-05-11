d116ee6ad0ead79498d6b725a3bde613d65d5866
Dave Nash <davidjnash2@gmail.com>
2023-05-11 11:44:45 -0500

    added event handler for submit button click, includes ajax logic for POST method to /guesses url, addGuess calls function getGuesses

---
+++ server/nodules/guesses.js
@@ -0,0 +1,5 @@
+let guesses = [];
+
+console.log(guesses);
+
+module.exports = guesses;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,41 @@
-$(document).ready(handleReady);
+const { response } = require("express");
 
-function handleReady() {
+$(document).ready(onReady);
+
+function onReady() {
   console.log("jquery is loaded!")
+
+  // add submit button collect input data
+  $('#submit-button').on('click', addGuess);
+
+  // getGuesses();
+
+}
+
+// defining event handler for click of #submit-button
+function addGuess(event){
+  event.preventDefault();
+  console.log( 'Guess added!' );
+  
+  const guess1 = $('#player-one-guess').val();
+  const guess2 = $('#player-two-guess').val();
+
+
+  $('#player-one-guess').val('');
+  $('#player-two-guess').val('');
+
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: {
+      guess1,
+      guess2
+    }
+  }).then(function(response){
+    console.log( 'Posted' );
+    getGuesses();
+  }).catch(function(error){
+    alert('Error with guess POST!');
+    console.log( 'Error with post:', error);
+  })
 }
